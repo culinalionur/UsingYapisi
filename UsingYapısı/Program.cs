468d990525497ebe660d3061a2bda907a524fa94
culinalionur <onurakinci53@gmail.com>
2023-01-02 17:54:19 +0300

    Add project files.

---
+++ UsingYapısı/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using UsingYapısı;
+
+using UsingDeneme usingDeneme = new UsingDeneme();
+Sum();
+void Sum()
+{
+    using UsingDeneme usingDeneme1 = new UsingDeneme();
+    usingDeneme1.Show();
+}
+
+UsingDeneme GenerateMetot()
+{
+    using UsingDeneme deneme = new UsingDeneme();
+    return deneme;
+}
+using UsingDeneme deneme2 = GenerateMetot();
+deneme2.Show();
---
+++ UsingYapısı/UsingDeneme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingYapısı
+{
+    public class UsingDeneme : IDisposable
+    {
+        public bool IsDisposed { get; private set; } = false;
+        public void Dispose()
+        {
+            IsDisposed= true; 
+        }
+        public void Show()
+        {
+            Console.WriteLine("Using yapısı denemesi yapılıyor.");
+            Console.WriteLine("Dispose status {0}", IsDisposed);
+        }
+    }
+}
